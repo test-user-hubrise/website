760a2cbeba7eab322ccfc74e5de010d4418a37fb
Paul Ermolin <oatto.paul@gmail.com>
2019-07-31 18:34:31 +0200

    chore: setup global styles + normalize + foundation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from "react"
+import Layout from "./src/components/layout"
 
-// You can delete this file if you're not using it
+import 'normalize.css'
+import './src/styles/global.scss'
+
+export function wrapPageElement({ element, props }) {
+  return (
+    <Layout {...props}>
+      {element}
+    </Layout>
+  )
+}
