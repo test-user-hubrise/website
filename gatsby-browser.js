be1496c7d596491423709b2b6fd25ee550ec10d6
Paul Ermolin <oatto.paul@gmail.com>
2019-08-01 11:52:15 +0200

    chore: pull in 'Poppins' font, font-awesome & motion-ui

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "./src/components/layout"
 
-import 'normalize.css'
 import './src/styles/global.scss'
 
 export function wrapPageElement({ element, props }) {
