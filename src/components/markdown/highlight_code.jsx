468885de2d4eceb2eb3d5a9afe29d1b0f2cd5024
Paul Ermolin <oatto.paul@gmail.com>
2019-08-07 21:11:36 +0200

    feat(mdx): use custom Prism instance & enable line numbering

--- src/components/markdown/highlight_code.jsx
+++ src/components/markdown/highlight_code.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import customTheme from 'prism-react-renderer/themes/github'
+import Prism from 'prismjs'
+import Highlight from 'prism-react-renderer'
 
 const HighlightCode = ({ code, language, inline }) => {
   if (inline) {
     return (
       <Highlight
         code={code}
-        {...defaultProps}
-        theme={customTheme}
+        language={language}
+        Prism={Prism}
       >
         {({ className, style }) => (
           <code
             className={`${className} documentation__code--inline`}
-            style={style}
+            style={{
+              ...style,
+              padding: `.1rem .3rem`,
+            }}
           >
             {code}
           </code>
-        )
-        }
+        )}
       </Highlight>
     )
   }
@@ -28,20 +30,22 @@ const HighlightCode = ({ code, language, inline }) => {
     <Highlight
       code={code}
       language={language}
-      {...defaultProps}
-      theme={customTheme}
+      Prism={Prism}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
-          {tokens.map((line, i) => !line[0].empty && (
-            <code
-              {...getLineProps({ line, key: i })}
-            >
-              {line.map((token, key) => token.content && (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </code>
-          ))}
+        <pre className={`${className} line-numbers`} style={style}>
+          <div className="prism-code__wrapper">
+            {tokens.map((line, i) => !line[0].empty && (
+              <span {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => token.content && (
+                  <span {...getTokenProps({ token, key })}/>
+                ))}
+              </span>
+            ))}
+            <span aria-hidden="true" className="line-numbers-rows">
+              {tokens.map((line) => !line[0].empty ? <span /> : null)}
+            </span>
+          </div>
         </pre>
       )}
     </Highlight>
