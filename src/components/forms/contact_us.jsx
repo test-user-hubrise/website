fe8102423e9d27b02d86277f7731701bef28c0c9
Paul Ermolin <oatto.paul@gmail.com>
2019-08-08 18:48:35 +0200

    refactor(contact_us): use formik+yup

--- src/components/forms/contact_us.jsx
+++ src/components/forms/contact_us.jsx
@@ -1,8 +1,108 @@
 import React, { useContext } from 'react'
+import { withFormik, Form, Field, ErrorMessage } from 'formik'
+import * as yup from 'yup'
 
 import AppContext from '../../context/AppContext'
 
-const ContactUs = () => {
+const fields = [
+  {
+    id: `name`,
+    name: `name`,
+    type: `text`,
+    placeholder: `Your name`,
+  },
+  {
+    id: `email`,
+    name: `email`,
+    type: `email`,
+    placeholder: `Your email`,
+  }
+]
+
+const ContactUsBase = (props) => {
+  const { errors, touched } = props
+  return (
+    <Form
+      id="contact-us__form"
+      className="form form_modal"
+      noValidate="novalidate"
+    >
+      <div
+        className="form__block-row"
+      >
+        {fields.map((fieldProps, idx) => (
+          <div
+            key={`${fieldProps.name}--${idx}`}
+            className={`form__block form__block_medium ${
+              touched[fieldProps.name] && (errors[fieldProps.name] ? 'error' : 'valid')
+            }`}
+          >
+            <label htmlFor={fieldProps.name} />
+            <Field
+              className="form__input"
+              {...fieldProps}
+            />
+            <ErrorMessage
+              name={fieldProps.name}
+              render={(msg) => <p className="error__message">{msg}</p>}
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        className={`form__block ${
+          touched.message && (errors.message ? 'error' : 'valid')
+        }`}
+      >
+        <label htmlFor="message" />
+        <Field
+          component="textarea"
+          id="message"
+          name="message"
+          className="form__textarea"
+          placeholder="Your message ..."
+        />
+        <ErrorMessage
+          name="message"
+          render={(msg) => <p className="error__message">{msg}</p>}
+        />
+      </div>
+      <button
+        type="submit"
+        name="submit"
+        className="form__button form__button_full-width form__button_modal"
+      >
+        Send
+      </button>
+    </Form>
+  )
+}
+
+const contactUsSchema = yup.object().shape({
+  name: yup.string()
+    .min(2, `Is your name really 1 character long? o_0`),
+  email: yup.string()
+    .email(`Provided email seems to be incorrect. Could you double check?`)
+    .required(`Sorry, but we cannot proceed without your email.`),
+  message: yup.string()
+    .min(10, `We'd really appreciate a message longer than 10 characters.`)
+    .required(`Sorry, but empty messages are no fun.`),
+})
+
+const ContactUs = withFormik({
+  mapPropsToValues: () => ({
+    name: ``,
+    email: ``,
+    message: ``,
+  }),
+  validationSchema: contactUsSchema,
+  handleSubmit: (_values, { resetForm }) => {
+    alert(`Let's pretend its sent!`)
+    resetForm()
+  },
+})(ContactUsBase)
+
+const Wrapper = () => {
   const { toggleContactUsVisibility } = useContext(AppContext)
   return (
     <div
@@ -28,62 +128,19 @@ const ContactUs = () => {
         <h5 className="modal__title">
           Contact Us
         </h5>
-        <form
-          id="contact-us__form"
-          className="form form_modal"
-          noValidate="novalidate"
-        >
-          <div className="form__block-row">
-            <div className="form__block form__block_medium">
-              <label htmlFor="name" />
-              <input
-                id="name"
-                className="form__input"
-                type="text"
-                placeholder="Your Name"
-                name="name"
-              />
-            </div>
-            <div className="form__block form__block_medium">
-              <label htmlFor="email" />
-              <input
-                id="email"
-                className="form__input"
-                type="email"
-                placeholder="Your Email"
-                name="email"
-              />
-            </div>
-          </div>
-          <div className="form__block">
-            <label htmlFor="message" />
-            <textarea
-              id="message"
-              className="form__textarea"
-              placeholder="Your Message..."
-              name="message"
-            />
-          </div>
-          <button
-            className="form__button form__button_full-width form__button_modal"
-            type="submit"
-            name="submit"
-          >
-            Send
-          </button>
-        </form>
+        <ContactUs />
         <button
-          className="close-button modal__close-button"
           type="button"
+          className="close-button modal__close-button"
           data-close=""
-          aria-label="Close"
+          aria-label="close"
           onClick={toggleContactUsVisibility}
         >
-          <i className="fa fa-close modal__close-button-icon"/>
+          <i className="fa fa-close modal__close-button-icon" />
         </button>
       </div>
     </div>
   )
 }
 
-export default ContactUs
+export default Wrapper
