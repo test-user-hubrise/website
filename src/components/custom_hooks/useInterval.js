5dd6e0f020c2ed8ec5b0c3ac1c4d749e44ab75e5
Paul Ermolin <oatto.paul@gmail.com>
2019-08-21 21:57:31 +0200

    feat: add custom hook -> useInterval

---
+++ src/components/custom_hooks/useInterval.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react'
+
+/**
+ * Wraps `setInterval`. Triggers the function each interval.
+ * @param {Function} fn function to call
+ * @param {number} delay in milliseconds
+ * @return {void}
+ */
+export function useInterval(fn, delay) {
+  const savedCallback = useRef()
+
+  useEffect(() => {
+    savedCallback.current = fn
+  })
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current()
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
