fb8f3cd51d37339ba5ecdeca1de9d4864d402f0d
Paul Ermolin <oatto.paul@gmail.com>
2019-08-22 11:45:51 +0200

    feat: add home page / FR

---
+++ src/pages/fr/index.jsx
@@ -0,0 +1,269 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { IndexPage as IndexBase } from '../'
+
+import { generateKey } from '../../components/utils'
+import { useInterval } from '../../components/custom_hooks'
+
+import diagram from '../../images/diagram_french.png'
+import carouselImg from '../../images/logo-carousel.jpg'
+import preview from '../../images/coming_soon.mp4'
+
+const pageContent = {
+  hero: {
+    title: `HubRise centralise les données de votre commerce`,
+    description: `Reliez votre caisse, votre site Internet et tous vos logiciels.`,
+    link: `En savoir plus`,
+  },
+  main: {
+    title: `L'intégration à portée de main`,
+    description: `HubRise centralise toutes vos données : commandes, clients, produits...
+Les applications raccordées à HubRise peuvent instantanément échanger ces données entre elles.`,
+    features: [
+      `Commande en ligne`,
+      `Logiciel de caisse`,
+      `Gestion de livreurs`,
+      `Carte de fidélité`,
+      `Envoi d'emails`,
+      `Applications mobiles`,
+      `Et plus encore`,
+    ],
+  },
+}
+
+const formContent = {
+  title: `Démarrez maintenant`,
+  description: `HubRise est gratuit jusqu'à 50 commandes par mois.`,
+  link: `Voir tarifs`,
+  placeholders: {
+    first_name: `Prénom`,
+    last_name: `Nom`,
+    email: `Email`,
+    password: `Mot de passe`,
+  },
+  button: `Créer votre compte`,
+}
+
+const Video = () => {
+  return (
+    <section className="section">
+      <div className="section__in section__in_padding section__in_reverse">
+        <h3 className="section__title">Démonstration</h3>
+        <video className="index-video" width="400" controls>
+          <source src={preview} />
+          Votre navigateur ne supporte pas l'affichage des vidéos HTML5.
+        </video>
+      </div>
+    </section>
+  )
+}
+
+const Faq = () => {
+  const links = [
+    `Every request, every bit of data is traced`,
+    `Access control with thin grained permissions`,
+    `The data belongs to you`,
+  ]
+  return (
+    <section className="section section_full-width section_padding">
+      <div className="section__in section__in_green section__in_padding">
+        <h3 className="section__title">F.A.Q.</h3>
+        <ul className="index-faq">
+          {links.map((link, idx) => (
+            <li key={generateKey(link, idx)} className="index-faq__item">
+              <a className="index-faq__link" href="/fr/faq">
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+const CompatibleApps = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [lastActiveIndex, setLastActiveIndex] = useState()
+  useInterval(function rotateBanners() {
+    setLastActiveIndex(activeIndex)
+    setActiveIndex(activeIndex >= lastIndex ? 0 : activeIndex + 1)
+  }, 5000)
+
+  const slides = [
+    {
+      title: `(1) Orderlord: a mobile app to manage your delivery fleet`,
+      img: carouselImg,
+      description: `First slide details.`,
+    },
+    {
+      title: `(2) Orderlord: a mobile app to manage your delivery fleet`,
+      img: carouselImg,
+      description: `Second slide details.`,
+    },
+    {
+      title: `(3) Orderlord: a mobile app to manage your delivery fleet`,
+      img: carouselImg,
+      description: `Second slide details.`,
+    },
+    {
+      title: `(4) Orderlord: a mobile app to manage your delivery fleet`,
+      img: carouselImg,
+      description: `Second slide details.`,
+    },
+  ]
+  const lastIndex = slides.length - 1
+
+  return (
+    <section className="section">
+      <div className="section__in section__in_padding section__in_reverse">
+        <h3 className="section__title">
+          De nouvelles applications tous les mois
+        </h3>
+        <p className="section__description">
+          HubRise est connecté avec un nombre croissant de solutions : JDC/Kezia
+          II, Nestor, MyOrderBox, OrderLord...
+          <br />
+          D'autres intégrations sont en cours.
+          <br />
+          <a className="section__description-link" href="/fr/apps">
+            Voir les applications disponibles
+          </a>
+          <span className="section__description-span">(commerçants)</span>-
+          <a className="section__description-link" href="/fr/developpeurs">
+            Connectez votre logiciel à HubRise
+          </a>
+          <span className="section__description-span">(développeurs)</span>
+        </p>
+        <div
+          className="index-carousel orbit-wrapper"
+          role="region"
+          aria-label="Favorite Space Pictures"
+        >
+          <ul
+            className="index-carousel__container orbit-container"
+            tabIndex="0"
+          >
+            <i
+              className="index-carousel__arrow index-carousel__arrow_previous orbit-previous fa fa-chevron-circle-left"
+              tabIndex="0"
+              onClick={() => {
+                setLastActiveIndex(activeIndex)
+                const newIndex = activeIndex - 1
+                setActiveIndex(newIndex < 0 ? lastIndex : newIndex)
+              }}
+            />
+            <i
+              className="index-carousel__arrow index-carousel__arrow_next orbit-next fa fa-chevron-circle-right"
+              tabIndex="0"
+              onClick={() => {
+                setLastActiveIndex(activeIndex)
+                const newIndex = activeIndex + 1
+                setActiveIndex(newIndex > lastIndex ? 0 : newIndex)
+              }}
+            />
+            {slides.map(({ title, img }, idx) => {
+              return (
+                <li
+                  key={generateKey(title, idx)}
+                  className="index-carousel__slide orbit-slide"
+                  style={{
+                    visibility: `${
+                      activeIndex === idx || lastActiveIndex === idx
+                        ? 'visible'
+                        : 'hidden'
+                    }`,
+                    transform: `translateX(${
+                      activeIndex === idx
+                        ? '0'
+                        : idx < activeIndex
+                        ? '-100'
+                        : '100'
+                    }%)`,
+                    transition: `transform .55s ease-out`,
+                  }}
+                >
+                  <span className="index-carousel__title">{title}</span>
+                  <img
+                    className="index-carousel__image orbit-image"
+                    src={img}
+                    alt="Space"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+          <nav className="index-carousel__bullets orbit-bullets">
+            {slides.map(({ description }, idx) => {
+              const isCurrentSlide = activeIndex === idx
+              return (
+                <button
+                  key={generateKey(description, idx)}
+                  className={`${isCurrentSlide ? 'is-active' : ''}`}
+                  onClick={() => {
+                    if (isCurrentSlide) return
+                    setLastActiveIndex(activeIndex)
+                    setActiveIndex(idx)
+                  }}
+                >
+                  <span className="show-for-sr">{description}</span>
+                  {isCurrentSlide && (
+                    <span className="show-for-sr">Current slide</span>
+                  )}
+                </button>
+              )
+            })}
+          </nav>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const Philosophy = () => {
+  return (
+    <section className="section">
+      <div className="section__in section__in_padding">
+        <h3 className="section__title">Qui sommes-nous ?</h3>
+        <p className="section__description section__description_black">
+          HubRise est une société basée en France, fondée par des ingénieurs
+          spécialisés dans les logiciels de réservation aérienne, et de commande
+          en ligne pour les restaurants.
+        </p>
+        <p className="section__description">
+          Notre vision : les commerçants utilisent de plus en plus
+          d’applications informatiques pour leur activité, mais celles-ci ne
+          communiquent généralement pas entre elles. HubRise veut offrir une
+          solution universelle permettant à toutes les applications des
+          commerçants de communiquer aisément entre elles.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+const IndexPage = (props) => {
+  return (
+    <IndexBase {...props}>
+      <Video />
+      <Faq />
+      <CompatibleApps />
+      <Philosophy />
+    </IndexBase>
+  )
+}
+
+IndexPage.propTypes = {
+  pageContent: PropTypes.objectOf(PropTypes.any).isRequired,
+  formContent: PropTypes.objectOf(PropTypes.any).isRequired,
+  diagram: PropTypes.string.isRequired,
+}
+
+export default () => (
+  <IndexPage
+    pageContent={pageContent}
+    formContent={formContent}
+    diagram={diagram}
+  />
+)
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Link from '../components/link'
 import SignupForm from '../components/forms/signup'
@@ -8,7 +9,7 @@ import { generateKey } from '../components/utils'
 import hero from '../images/hero_image_optimized.jpg'
 import diagram from '../images/diagram.png'
 
-const content = {
+const pageContent = {
   hero: {
     title: `Centralize the data of your retail store`,
     description: `Connect your POS, your website and all your applications.`,
@@ -31,61 +32,80 @@ const content = {
   },
 }
 
-export const IndexPage = ({ content }) => (
-  <>
-    <div
-      className="index-hero"
-      style={{
-        backgroundImage: `url(${hero})`,
-        backgroundRepeat: `no-repear`,
-        backgroundSize: `cover`,
-      }}
-    >
-      <div className="index-hero__container">
-        <div className="index-hero__banner">
-          <div className="index-hero__banner-in">
-            <h3 className="index-hero__title">{content.hero.title}</h3>
-            <p className="index-hero__description">
-              {content.hero.description}
-              <Link className="index-hero__link" to="#more">
-                {content.hero.link}
-              </Link>
-            </p>
+const formContent = {
+  title: `Get started now`,
+  description: `HubRise is free up to 50 orders per month.`,
+  link: `See pricing`,
+  placeholders: {
+    first_name: `First name`,
+    last_name: `Last name`,
+    email: `Email`,
+    password: `Password`,
+  },
+  button: `Create your account`,
+}
+
+export const IndexPage = ({ pageContent, formContent, diagram, children }) => {
+  return (
+    <>
+      <div
+        className="index-hero"
+        style={{
+          backgroundImage: `url(${hero})`,
+          backgroundRepeat: `no-repear`,
+          backgroundSize: `cover`,
+        }}
+      >
+        <div className="index-hero__container">
+          <div className="index-hero__banner">
+            <div className="index-hero__banner-in">
+              <h3 className="index-hero__title">{pageContent.hero.title}</h3>
+              <p className="index-hero__description">
+                {pageContent.hero.description}
+                <Link className="index-hero__link" to="#more">
+                  {pageContent.hero.link}
+                </Link>
+              </p>
+            </div>
+          </div>
+          <SignupForm content={formContent} />
+        </div>
+      </div>
+      <section id="more" className="section">
+        <div className="section__in section__in_padding">
+          <h3 className="section__title">{pageContent.main.title}</h3>
+          <p className="section__description">{pageContent.main.description}</p>
+          <ul className="index-about">
+            {pageContent.main.features.map((feature, idx) => (
+              <li key={generateKey(feature, idx)} className="index-about__item">
+                <span className="index-about__span">{feature}</span>
+              </li>
+            ))}
+          </ul>
+          <div className="section__diagram">
+            <img
+              className="section__diagram-image"
+              src={diagram}
+              alt="diagram"
+            />
           </div>
         </div>
-        <SignupForm
-          content={{
-            title: `Get started now`,
-            description: `HubRise is free up to 50 orders per month.`,
-            link: `See pricing`,
-            placeholders: {
-              first_name: `First name`,
-              last_name: `Last name`,
-              email: `Email`,
-              password: `Password`,
-            },
-            button: `Create your account`,
-          }}
-        />
-      </div>
-    </div>
-    <section id="more" className="section">
-      <div className="section__in section__in_padding">
-        <h3 className="section__title">{content.main.title}</h3>
-        <p className="section__description">{content.main.description}</p>
-        <ul className="index-about">
-          {content.main.features.map((feature, idx) => (
-            <li key={generateKey(feature, idx)} className="index-about__item">
-              <span className="index-about__span">{feature}</span>
-            </li>
-          ))}
-        </ul>
-        <div className="section__diagram">
-          <img className="section__diagram-image" src={diagram} alt="diagram" />
-        </div>
-      </div>
-    </section>
-  </>
-)
+      </section>
+      {children}
+    </>
+  )
+}
 
-export default () => <IndexPage content={content} />
+IndexPage.propTypes = {
+  pageContent: PropTypes.objectOf(PropTypes.any).isRequired,
+  formContent: PropTypes.objectOf(PropTypes.any).isRequired,
+  diagram: PropTypes.string.isRequired,
+}
+
+export default () => (
+  <IndexPage
+    pageContent={pageContent}
+    formContent={formContent}
+    diagram={diagram}
+  />
+)
