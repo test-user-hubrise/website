31d411697773a41fb888c1c16786b82b12c4c703
Paul Ermolin <oatto.paul@gmail.com>
2019-09-03 15:14:30 +0200

    feat: use i18next plugin which lazily loads translation namespaces

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,20 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import backend from 'i18next-xhr-backend'
 
 i18n
+  .use(backend)
   .use(initReactI18next)
   // All options: https://www.i18next.com/overview/configuration-options
   .init({
-    resources: {},
-    lng: 'en',
-    fallbackLng: 'en',
+    lng: `en`,
+    fallbackLng: `en`,
     debug: true,
     interpolation: {
       escapeValue: false
+    },
+    react: {
+      useSuspense: false
     }
   })
 
