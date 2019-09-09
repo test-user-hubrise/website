30bd80913430b7e00da1068d598a2c78e467b293
Paul Ermolin <oatto.paul@gmail.com>
2019-09-09 13:24:33 +0200

    refactor(i18n): import translations at the initialization stage to avoid download delays
    + configuration

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,21 +1,31 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import backend from 'i18next-xhr-backend'
+
+import homeEn from '../../public/locales/en/home'
+import formsEn from '../../public/locales/en/forms'
+import homeFr from '../../public/locales/fr/home'
+import formsFr from '../../public/locales/fr/forms'
 
 i18n
-  .use(backend)
   .use(initReactI18next)
   // All options: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: {
+      en: {
+        home: homeEn,
+        forms: formsEn
+      },
+      fr: {
+        home: homeFr,
+        forms: formsFr
+      }
+    },
     lng: `en`,
     fallbackLng: `en`,
-    debug: false,
-    interpolation: {
-      escapeValue: false
-    },
-    react: {
-      useSuspense: false
-    }
+    debug: true,
+    interpolation: { escapeValue: false },
+    returnObjects: true,
+    react: { transSupportBasicHtmlNodes: false }
   })
 
 export default i18n
