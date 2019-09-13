3d0024a1f7b844dcdebb0c6ed20355f148449a8a
Paul Ermolin <oatto.paul@gmail.com>
2019-09-13 12:53:04 +0200

    test(e2e): add useful checkups to smoke test

--- cypress/integration/smoke.js
+++ cypress/integration/smoke.js
@@ -2,4 +2,24 @@ describe(`website`, () => {
   it(`doesn't crash and burn`, () => {
     cy.visit(`/`)
   })
+
+  it(`renders elements, allows interaction and navigation between various pages`, () => {
+    cy.viewport(1024, 660)
+
+    cy.visit(`/`)
+    cy.contains(`form`, `Create your account`)
+    cy.contains(`HubRise makes POS integration easy`)
+    cy.contains(`li`, `Developers`).click()
+    cy.contains(`Contact us`).click()
+    cy.get(`div[role="dialog"]`).should(`be.visible`)
+
+    cy.visit(`/fr`)
+    cy.contains(`form`, `Créer votre compte`)
+    cy.contains(`li`, `Apps`).click()
+    cy.contains(`LivePepper`)
+
+    cy.visit(`/fr/api/catalog-management`)
+    cy.get(`h2#skus a`).click()
+    cy.url().should(`include`, `#skus`)
+  })
 })
