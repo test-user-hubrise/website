e2f7da50575f9ba4a208ff57b9ce74e00c650df5
Paul Ermolin <oatto.paul@gmail.com>
2019-09-13 15:30:53 +0200

    test(unit): jest & friends - installation and initial setup

---
+++ jest/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rootDir: process.cwd(),
+  testMatch: [`**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)`],
+  transform: {
+    '^.+\\.jsx?$': `<rootDir>/jest/preprocess.js`
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/jest/__mocks__/file_mock.js`
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: { __PATH_PREFIX__: `` },
+  setupFiles: [`<rootDir>/jest/loadershim.js`],
+  setupFilesAfterEnv: [`<rootDir>/jest/setup.js`]
+}
