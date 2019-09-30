9bedd55a3f539efc3d32626b66f36e0c0b689b49
Paul Ermolin <oatto.paul@gmail.com>
2019-09-30 18:33:39 +0200

    refactor: arrange info section of app's overview page

--- src/templates/app/info.jsx
+++ src/templates/app/info.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 
+import Link from '../../components/link'
+import { generateKey, splitCamelCase } from '../../components/utils'
+
 const Info = ({ content }) => {
   return (
-    <div
-      className='section__content'
-    >
+    <div className='section__content'>
       <ul>
-        {Object.entries(content).map(([ key, value ], idx) => (
-          <li key={idx}>
-            {key}: {value}
-          </li>
-        ))}
+        {Object.entries(content).map(([ label, value ]) => {
+          const labelWithSpaces = splitCamelCase(label)
+          const adjustedLabel = labelWithSpaces[0].toUpperCase() + labelWithSpaces.slice(1)
+
+          return (
+            <li key={generateKey(label, value)}>
+              {adjustedLabel}
+              :{` `}
+              <span style={{ fontWeight: 500 }}>
+                {label === `website`
+                  ? (
+                    <Link to={value}>
+                      {value}
+                    </Link>
+                  )
+                  : value
+                }
+              </span>
+            </li>
+          )
+        }
+        )}
       </ul>
     </div>
   )
