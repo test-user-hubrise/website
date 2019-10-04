f5d1952b2e3c903105ec5686499d0b97da9ab4e0
Paul Ermolin <oatto.paul@gmail.com>
2019-10-04 12:28:18 +0200

    tests(unit): cover i18n utils

---
+++ src/i18n/__tests__/utils.test.js
@@ -0,0 +1,15 @@
+import { getLanguage } from '../utils'
+import locales from '../locales'
+
+describe(`getLanguage`, () => {
+  it(`should return default language code if path prefix is absent`, () => {
+    const [{ code: defaultLanguageCode }] = Object.values(locales)
+      .filter((info) => info.default === true)
+
+    expect(getLanguage(`/api/general-concepts`)).toBe(defaultLanguageCode)
+  })
+
+  it(`should return respective language code based on path prefix`, () => {
+    expect(getLanguage(`/fr/apps/ikentoo`)).toBe(`fr`)
+  })
+})
